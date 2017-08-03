b999bd7df0a5a4e791242d27a878fc26609d94d7
Tito <htp.tito@gmail.com>
2017-08-02 22:01:01 -0700

    Added new scriptable object CardData assets.

---
+++ Assets/Editor/CardDataAsset.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class CardDataAsset
+{
+	[MenuItem("Assets/Create/Card Data")]
+	public static void CreateAsset ()
+	{
+		ScriptableObjectUtility.CreateAsset<CardData> ();
+	}
+}
---
+++ Assets/Scripts/ScriptableObjects/CardData.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class CardData : ScriptableObject 
+{
+	public enum CardType
+	{
+		None, Blessing, Curse
+	}
+
+	public enum CardTarget
+	{
+		Any, Viking, Zombie
+	}
+
+	public int id;
+	[TextArea(2,4)]
+	public string description;
+	public bool isQuick;
+	public CardType type;
+	public CardTarget target;
+}

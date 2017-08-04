3ec2ae5109f34ecccd0dc1c58ec8b4af24b7d052
Tito <htp.tito@gmail.com>
2017-08-03 23:38:35 -0700

    Added first 25 blessing cards.

--- Assets/Scripts/ScriptableObjects/CardData.cs
+++ Assets/Scripts/ScriptableObjects/CardData.cs
@@ -12,10 +12,13 @@ public class CardData : ScriptableObject
 		Any, Viking, Zombie
 	}
 
+	[Header("General Info")]
 	public int id;
 	[TextArea(2,4)]
 	public string description;
 	public bool isQuick;
 	public CardType type;
 	public CardTarget target;
+	public Sprite cardImage;
+
 }

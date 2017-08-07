31916da49b91c50315a354416760f40e6d343f78
Tito <htp.tito@gmail.com>
2017-08-06 17:35:50 -0700

    Added Editor Tool menu "VvZ Tools". Added scriptable wizards Editor tool to select all game objects of any given tag in the hierarchy.

---
+++ Assets/Editor/SelectAllOfTag.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEditor;
+
+public class SelectAllOfTag : ScriptableWizard {
+
+	public string searchTag = "Tag Here!";
+
+	[MenuItem("VvZ Tools/Select All Of Tag...")]
+	static void SelectAllOfTagWizard()
+	{
+		ScriptableWizard.DisplayWizard<SelectAllOfTag> ("Select All Of Tag...","Make Selection");
+	}
+
+	void OnWizardCreate()
+	{
+		GameObject[] gameobjects = GameObject.FindGameObjectsWithTag(searchTag);
+		Selection.objects = gameobjects;
+	}
+}

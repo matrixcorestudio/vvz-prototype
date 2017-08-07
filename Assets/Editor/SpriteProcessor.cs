27bb7057d12d24c589e4b57e6e1c21811a3fdb77
Tito <htp.tito@gmail.com>
2017-08-06 17:07:32 -0700

    Added sprite importing Editor tool. Any image imported in sprites directory will automatically set as texture type: sprite 2D.

---
+++ Assets/Editor/SpriteProcessor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEditor;
+
+public class SpriteProcessor : AssetPostprocessor {
+
+	void OnPostprocessTexture (Texture2D texture)
+	{
+		string lowercaseAssetPath = assetPath.ToLower();
+		bool isInSpriteDirectory = lowercaseAssetPath.IndexOf("/sprites/") != -1;
+		if(isInSpriteDirectory)
+		{
+			TextureImporter textureImporter = assetImporter as TextureImporter;
+			textureImporter.textureType = TextureImporterType.Sprite;
+		}
+	}
+}

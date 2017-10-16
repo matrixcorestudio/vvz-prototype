8ff73748f71ee0e4bd88cbe3562b55ff76d8c22a
CARE-DESK-06\Humberto <humberto.trujillo@uabc.edu.mx>
2017-10-16 18:18:39 +0900

    Character drag fixed. New baked lighting data for lobby scene.

--- Assets/_Scripts/Player/Player.cs
+++ Assets/_Scripts/Player/Player.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Networking;
@@ -13,25 +12,25 @@ namespace Prototype.Player
 
     public class Player : NetworkBehaviour
     {
-        private string[] vikingNamesFlight1 = { "[L] Storm Caller", "Dice Master", "Gambler", "Earl Stone" };
-        private string[] zombieNamesFlight1 = { "[L] Puniszher", "Crawler", "Lizard Tongue", "Life-Taker" };
-
-        private Color[] vikingColorsFlight1 = { Color.white, new Color(1f, 165f / 255f, 0), Color.red, Color.black};
-        private Color[] zombieColorsFlight1 = { Color.magenta, new Color(165f / 255f, 42f / 255f, 42f / 255f), Color.green, Color.yellow};
-
-        public List<Text> vikingNames;
-        public List<Text> zombieNames;
-        public GameObject vikingPlayer;
-        public GameObject zombiePlayer;
-
         [SyncVar] public new string name;
         [SyncVar] public Color playerTextColor;
 
-        private Enums.PlayerType playerType;
+        [SerializeField] ToggleEvent onToggleShared;
+        [SerializeField] ToggleEvent onToggleLocal;
+        [SerializeField] ToggleEvent onToggleRemote;
 
-        [Header("Sprites")]
-        [SerializeField] SpriteRenderer vikingSprite;
-        [SerializeField] SpriteRenderer zombieSprite;
+        [SerializeField] List<Text> characterNames;
+        [SerializeField] List<SpriteRenderer> renderers;
+        [SerializeField] Sprite vikingSprite;
+        [SerializeField] Sprite zombieSprite;
+
+        private Enums.PlayerType playerType;
+        private string[] vikingNamesFlight1 = { "[L] Storm Caller", "Dice Master", "Gambler", "Earl Stone" };
+        private string[] zombieNamesFlight1 = { "[L] Puniszher", "Crawler", "Lizard Tongue", "Life-Taker" };
+        private Color[] vikingColorsFlight1 = { Color.white, new Color(1f, 165f / 255f, 0), Color.red, Color.black};
+        private Color[] zombieColorsFlight1 = { Color.magenta, new Color(165f / 255f, 42f / 255f, 42f / 255f), Color.green, Color.yellow};
+
+        
 
         private void Start()
         {
@@ -50,47 +49,72 @@ namespace Prototype.Player
                 playerType = Enums.PlayerType.Spectator;
                 InitializeSpectator();
             }
+            EnablePlayer();
+        }
+
+        public void EnablePlayer()
+        {
+            onToggleShared.Invoke(true);
+            if (isLocalPlayer)
+            {
+                onToggleLocal.Invoke(true);
+            }
+            else
+            {
+                onToggleRemote.Invoke(true);
+            }
+        }
+
+        public void DisablePlayer()
+        {
+            onToggleShared.Invoke(false);
+            if (isLocalPlayer)
+            {
+                onToggleLocal.Invoke(false);
+            }
+            else
+            {
+                onToggleRemote.Invoke(false);
+            }
         }
 
         private void InitializeVikings()
         {
-            gameObject.GetComponent<NetworkTransformChild>().target = vikingPlayer.transform;
-            zombiePlayer.GetComponent<ZombiePlayer>().DestroyZombies();
             for (int i = 0; i < vikingNamesFlight1.Length; ++i)
             {
-                vikingNames[i].text = vikingNamesFlight1[i];
-                vikingNames[i].color = vikingColorsFlight1[i];
-            }
-
-            vikingPlayer.GetComponent<VikingPlayer>().EnablePlayer();
-
-            if (isLocalPlayer)
-            {
-                vikingSprite.sortingOrder = -1;
+                characterNames[i].text = vikingNamesFlight1[i];
+                characterNames[i].color = vikingColorsFlight1[i];
+                renderers[i].sprite = vikingSprite;
+                if (isLocalPlayer)
+                {
+                    renderers[i].sortingOrder = -1;
+                }
             }
         }
 
         private void InitializeZombies()
         {
-            gameObject.GetComponent<NetworkTransformChild>().target = zombiePlayer.transform;
-            vikingPlayer.GetComponent<VikingPlayer>().DestroyVikings();
             for (int i = 0; i < zombieNamesFlight1.Length; ++i)
             {
-                zombieNames[i].text = zombieNamesFlight1[i];
-                zombieNames[i].color = zombieColorsFlight1[i];
-            }
-
-            zombiePlayer.GetComponent<ZombiePlayer>().EnablePlayer();
-
-            if (isLocalPlayer)
-            {
-                zombieSprite.sortingOrder = -1; 
+                characterNames[i].text = zombieNamesFlight1[i];
+                characterNames[i].color = zombieColorsFlight1[i];
+                renderers[i].sprite = zombieSprite;
+                if (isLocalPlayer)
+                {
+                    renderers[1].sortingOrder = -1;
+                }
             }
         }
 
         private void InitializeSpectator()
         {
-
+            //if(isLocalPlayer)
+            //{
+            //    foreach (GameObject character in transform)
+            //    {
+            //        Network.Destroy(character);
+            //    }
+            //}
         }
     } 
 }

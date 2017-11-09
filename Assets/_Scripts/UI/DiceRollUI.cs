0fc27035b0e1cad2c620f944a26aeb4c8b71a01a
Luis Ochoa <ph.lwyz@gmail.com>
2017-11-08 21:23:55 -0800

    Viking and Zombie roll buttons now working and showing results in panel.

--- Assets/_Scripts/UI/DiceRollUI.cs
+++ Assets/_Scripts/UI/DiceRollUI.cs
@@ -8,52 +8,107 @@ public class DiceRollUI : Singleton<DiceRollUI>
 {
 	[SerializeField] GameObject rollPanel;
 	[SerializeField] Text rollTypeText;
-	[SerializeField] Text rollValueText;
 	[SerializeField] Text rollStatusText;
     [SerializeField] Text[] charNameTexts;
-	[SerializeField] float randomizerTime = 0.2f;
+    [SerializeField] Text[] rollValueText;
+    [SerializeField] float randomizerTime = 0.2f;
 
 	int lastRollValue = 0;
-	string lastRollType = string.Empty;
-    bool isSingle;
+    int[] lastRollValues = new int[4];
+    string lastRollType = string.Empty;
+	string lastDiceType = string.Empty;
+    string[] lastDiceTypes = new string[4];
 
     private void Start()
     {
         rollStatusText.text = string.Empty;
         rollTypeText.text = string.Empty;
-        rollValueText.text = string.Empty;
         for (int i = 0; i < charNameTexts.Length; ++i)
         {
             charNameTexts[i].text = string.Empty;
+            rollValueText[i].text = string.Empty;
+            lastDiceTypes[i] = string.Empty;
+            lastRollValues[i] = 0;
         }
     }
 
-    public void RollDice (int rollvalue, string diceType, Enums.RollType rollType)
+    public void RollSingleDice (int rollValue, string diceType)
 	{
-		lastRollValue = rollvalue;
-		lastRollType = diceType;
+		lastRollValue = rollValue;
+		lastDiceType = diceType;
 		rollPanel.SetActive(true);
-		rollTypeText.text = rollType.ToString();
-		StartCoroutine(VisualRandomizerRoutine(rollvalue));
-		UpdateRollStatusUI();
+        rollTypeText.text = Enums.RollType.SingleRoll.ToString();
+        lastRollType = Enums.RollType.SingleRoll.ToString();
+        charNameTexts[0].text = "Single Roll: ";
+        StartCoroutine(SingleVisualRandomizerRoutine());
 	}
 
-	void UpdateRollStatusUI ()
+    public void RollMultipleDice(int[] rollValues, string[] diceTypes, string[] charNames, Enums.RollType rollType)
+    {
+        lastRollValues = rollValues;
+        lastDiceTypes = diceTypes;
+        rollPanel.SetActive(true);
+        rollTypeText.text = rollType.ToString();
+        lastRollType = rollType.ToString();
+        for (int i = 0; i < charNameTexts.Length; ++i)
+        {
+            charNameTexts[i].text = charNames[i] + ": ";
+        }
+        StartCoroutine(MultipleVisualRandomizerRoutine());
+    }
+
+	void UpdateRollStatusUI (bool isMultipleRoll)
 	{
-		rollStatusText.text = "Last roll value: "+ lastRollValue+"; "+
-			"With dice type: " + lastRollType+"; "+
-			"By Player: "+ rollTypeText.text;
+        if (isMultipleRoll)
+        {
+            rollStatusText.text = "Last roll values: ";
+            for (int i = 0; i < rollValueText.Length; ++i)
+            {
+                rollValueText[i].text = (lastRollValues[i] < 0 ? "None" : lastRollValues[i].ToString());
+                if (lastDiceType == Enums.DiceType.CoinFlip.ToString())
+                {
+                    rollValueText[i].text = (lastRollValues[i] == 2 ? "Good" : "Bad");
+                }
+                rollStatusText.text += rollValueText[i].text + ", ";
+            }
+            rollStatusText.text += "with roll type: " + lastRollType;            
+        }
+        else
+        {
+            rollValueText[0].text = (lastRollValue < 0 ? "None" : lastRollValue.ToString());
+            if (lastDiceType == Enums.DiceType.CoinFlip.ToString())
+            {
+                rollValueText[0].text = (lastRollValue == 2 ? "Good" : "Bad");
+            }
+            rollStatusText.text = string.Format("Last roll value: {0}, with dice type: {1}, with " +
+                "roll type: {2}", rollValueText[0].text, lastDiceType, lastRollType);
+        }
 	}
 
-	IEnumerator VisualRandomizerRoutine (int rollvalue)
+	IEnumerator SingleVisualRandomizerRoutine ()
 	{
 		float elapsedTime = 0;
 		while (elapsedTime < randomizerTime)
 		{
 			yield return new WaitForSeconds(0.1f);
-			rollValueText.text = Random.Range(1,12).ToString();
+			rollValueText[0].text = Random.Range(1,12).ToString();
 			elapsedTime += Time.deltaTime;
 		}
-		rollValueText.text = rollvalue.ToString();
-	}
+        UpdateRollStatusUI(false);
+    }
+
+    IEnumerator MultipleVisualRandomizerRoutine()
+    {
+        float elapsedTime = 0;
+        while (elapsedTime < randomizerTime)
+        {
+            yield return new WaitForSeconds(0.1f);
+            for (int i = 0; i < rollValueText.Length; ++i)
+            {
+                rollValueText[i].text = Random.Range(1, 12).ToString(); 
+            }
+            elapsedTime += Time.deltaTime;
+        }
+        UpdateRollStatusUI(true);
+    }
 }
--- Assets/_Scripts/UI/DiceRollUIManager.cs
+++ Assets/_Scripts/UI/DiceRollUIManager.cs
@@ -14,6 +14,12 @@ public class DiceRollUIManager : NetworkBehaviour
     public delegate void OnDiceRoll(string result);
     public event OnDiceRoll DiceRollEvent;
 
+    private Enums.DiceType[] vikingDiceTypes;
+    private Enums.DiceType[] zombieDiceTypes;
+    private string[] vikingNamesFlight1 = { "[1] Storm Caller", "[2] Dice Master", "[3] Gambler", "[4] Earl Stone" };
+    private string[] zombieNamesFlight1 = { "[1] Puniszher", "[2] Crawler", "[3] Lizard Tongue", "[4] Life-Taker" };
+
+
     private void Awake()
     {
         List<string> diceOptions = new List<string>();
@@ -28,12 +34,39 @@ public class DiceRollUIManager : NetworkBehaviour
             charDiceDropdowns[i].ClearOptions();
             charDiceDropdowns[i].AddOptions(diceOptions);
         }
+
+        vikingDiceTypes = new Enums.DiceType[4];
+        zombieDiceTypes = new Enums.DiceType[4];
+
+        //Hardcoded for flight 1
+        vikingDiceTypes[0] = Enums.DiceType.D6Plus2;
+        vikingDiceTypes[1] = Enums.DiceType.D6Plus2;
+        vikingDiceTypes[2] = Enums.DiceType.D12Min3;
+        vikingDiceTypes[3] = Enums.DiceType.D10Max8;
+
+        //Hardcoded for flight 1
+        zombieDiceTypes[0] = Enums.DiceType.D6;
+        zombieDiceTypes[1] = Enums.DiceType.D4X2;
+        zombieDiceTypes[2] = Enums.DiceType.D6;
+        zombieDiceTypes[3] = Enums.DiceType.D6;
     }
     
     public void SingleRoll()
     {
         Debug.Log("Single Roll Button pressed ;) value: " + singleDiceDropdown.value);
         CmdRollDice(Enums.RollType.SingleRoll, (Enums.DiceType)singleDiceDropdown.value);
+    }
+
+    public void VikingRoll()
+    {
+        Debug.Log("Viking Roll button pressed!");
+        CmdRollDice(Enums.RollType.VikingRoll, Enums.DiceType.None);
+    }
+
+    public void ZombieRoll()
+    {
+        Debug.Log("Zombie Roll button pressed!");
+        CmdRollDice(Enums.RollType.ZombieRoll, Enums.DiceType.None);
     }
 
     [Server]
@@ -82,18 +115,50 @@ public class DiceRollUIManager : NetworkBehaviour
     [Command]
     void CmdRollDice(Enums.RollType rollType, Enums.DiceType diceType)
     {
-        //TODO: change this stuff to consider roll typ (single, vikings or zombies type of roll)
-        int rollResult = CalculateResult(diceType);
-        if (DiceRollEvent != null)
+        if (rollType == Enums.RollType.SingleRoll)
         {
-            DiceRollEvent("Dice Roll: " + rollResult + ", DiceType: " + diceType);
+            int rollResult = CalculateResult(diceType);
+            if (DiceRollEvent != null)
+            {
+                DiceRollEvent("Dice Roll: " + rollResult + ", DiceType: " + diceType);
+            }
+            RpcRollSingleDice(rollResult, diceType); 
         }
-        RpcRollDice(rollResult, diceType, rollType);
+        else
+        {
+            int[] rollResults = new int[4];
+            string[] diceTypes = new string[4];
+
+            if (rollType == Enums.RollType.VikingRoll)
+            {
+                for (int i = 0; i < vikingDiceTypes.Length; ++i)
+                {
+                    rollResults[i] = CalculateResult(vikingDiceTypes[i]);
+                    diceTypes[i] = vikingDiceTypes[i].ToString();
+                }
+                RpcRollMultipleDice(rollResults, diceTypes, vikingNamesFlight1, Enums.RollType.VikingRoll);
+            }
+            else //Roll type is zombies
+            {
+                for (int i = 0; i < zombieDiceTypes.Length; ++i)
+                {
+                    rollResults[i] = CalculateResult(zombieDiceTypes[i]);
+                    diceTypes[i] = zombieDiceTypes[i].ToString();
+                }
+                RpcRollMultipleDice(rollResults, diceTypes, zombieNamesFlight1, Enums.RollType.ZombieRoll);
+            }
+        }
     }
 
     [ClientRpc]
-    void RpcRollDice(int rollValue, Enums.DiceType diceType, Enums.RollType rollType)
+    void RpcRollSingleDice(int rollValue, Enums.DiceType diceType)
     {
-        DiceRollUI.Instance.RollDice(rollValue, diceType.ToString(), rollType);
+        DiceRollUI.Instance.RollSingleDice(rollValue, diceType.ToString());
+    }
+
+    [ClientRpc]
+    void RpcRollMultipleDice(int[] rollValues, string[] diceTypes, string[] charNames, Enums.RollType rollType)
+    {
+        DiceRollUI.Instance.RollMultipleDice(rollValues, diceTypes, charNames, rollType);
     }
 }

c147610255cbd78bd9edba7bfdaa77cd14a19989
Luis Ochoa <ph.lwyz@gmail.com>
2017-11-12 16:46:47 -0800

    -Modified and Integrated Ztriker´s UI to Game scene. -Added the Zombie Pool to Game scene. -Added a small draggable info panel to Game scene.

---
+++ Assets/_Scripts/UI/UIComponentDrag.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIComponentDrag : MonoBehaviour
+{
+    public void OnDrag()
+    {
+        transform.position = Input.mousePosition;
+    }
+}

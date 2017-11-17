bac6c1743164635f72381e4b8f0c61216e2e0ad6
Luis Ochoa <ph.lwyz@gmail.com>
2017-11-16 23:36:43 -0800

    HUDManager update

--- Assets/_Scripts/HUD.cs
+++ Assets/_Scripts/HUD.cs
@@ -21,18 +21,20 @@ public class HUD : NetworkBehaviour
     public event OnStFxTurnsChange StFxTurnsChangedEvent;
 
 
-    public string[] vikingNamesFlight1 = { "[1] Storm Caller", "[2] Dice Master", "[3] Gambler", "[4] Earl Stone" };
-    public string[] zombieNamesFlight1 = { "[1] Puniszher", "[2] Crawler", "[3] Lizard Tongue", "[4] Life-Taker" };
+    public string[] NamesFlight1 = { "[1] Storm Caller", "[2] Dice Master", "[3] Gambler", "[4] Earl Stone"
+                               ,"[1] Puniszher", "[2] Crawler", "[3] Lizard Tongue", "[4] Life-Taker" };
+    //public string[] zombieNamesFlight1 = { "[1] Puniszher", "[2] Crawler", "[3] Lizard Tongue", "[4] Life-Taker" };
 
-    public SyncListString CharacterNames = new SyncListString();
-    public SyncListInt Stocks = new SyncListInt();
-    public SyncListInt EnergyPower = new SyncListInt();
+    public SyncListString CharacterNames = new SyncListString(); //size 8
+    public SyncListInt Stocks = new SyncListInt(); //size 8
+    public SyncListInt EnergyPower = new SyncListInt(); //size 8
     public SyncListInt StFxs = new SyncListInt(); // size 16
     public SyncListInt StFxsTurns = new SyncListInt(); //size 16
 
     private Player player;
-    private bool foundHUDMngr;
     private bool foundPlayer = false;
+    private bool foundHUDMngr;
+    private HUDManager hUDManager;
 
     private void Start()
     {
@@ -49,14 +51,94 @@ public class HUD : NetworkBehaviour
             return;
         }
 
-        var hudMngr = FindObjectOfType<HUDManager>();
-        if (hudMngr != null)
+        hUDManager = FindObjectOfType<HUDManager>();
+        if (hUDManager != null)
         {
+            Debug.Log("Found HUD Manager");
             foundHUDMngr = true;
-            hudMngr.Init(this);
+            hUDManager.Init(this);
         }
+    }
 
+    [ClientCallback]
+    public void ChangeCharacterName(int index, string newName)
+    {
+        
+    }
 
+    [ClientCallback]
+    public void AddStock(int index)
+    {
+        Debug.Log("Adding 1 stock to viking leader");
+        CmdAddStock(index);
+    }
+
+    [ClientCallback]
+    public void RemoveStock(int index)
+    {
+
+    }
+
+    [ClientCallback]
+    public void AddEnergy(int index)
+    {
+
+    }
+
+    [ClientCallback]
+    public void RemoveEnergy(int index)
+    {
+
+    }
+
+    [ClientCallback]
+    public void ChangeStatusEffect(int index, Enums.StatusEffect statusEffect)
+    {
+
+    }
+
+    [ClientCallback]
+    public void AddTurnsToStatusEffect(int index)
+    {
+
+    }
+
+    [ClientCallback]
+    public void RemoveTurnsToStatusEffect(int index)
+    {
+
+    }
+
+    [Command]
+    private void CmdChangeCharacterName(int index, string newName)
+    {
+        CharacterNames[index] = newName;
+        if (NameChangedEvent != null)
+        {
+            NameChangedEvent(index);
+        }
+    }
+
+    [Command]
+    private void CmdAddStock(int index)
+    {
+        ++Stocks[index];
+        RpcAddStock(index);
+    }
+
+    [ClientRpc]
+    private void RpcAddStock(int index)
+    {
+        if (StockChangedEvent != null)
+        {
+            StockChangedEvent(index);
+        }
+        //if (hUDManager == null)
+        //{
+        //    hUDManager = FindObjectOfType<HUDManager>();
+        //}
+        //hUDManager.UpdateStocks(index);
+        //HUDManager.Instance.UpdateStocks(index);
     }
 
     [Server]
@@ -64,8 +146,8 @@ public class HUD : NetworkBehaviour
     {
         for (int i = 0; i < 8; ++i)
         {
-            CharacterNames.Add(i < 4 ? vikingNamesFlight1[i] : zombieNamesFlight1[i]);
-            Stocks.Add(i < 4 ? (i == 1 ? 4 : 3) : (i == 4 ? 6 : 5));
+            CharacterNames.Add(i < 4 ? NamesFlight1[i] : NamesFlight1[i]);
+            Stocks.Add(i < 4 ? (i == 0 ? 4 : 3) : (i == 4 ? 6 : 5));
             EnergyPower.Add(i < 4 ? 5 : 0);            
         }
 
--- Assets/_Scripts/UI/HUDManager.cs
+++ Assets/_Scripts/UI/HUDManager.cs
@@ -9,6 +9,9 @@ using Prototype.Player;
 
 public class HUDManager : MonoBehaviour
 {
+    public GameObject VikingInfo;
+    public GameObject ZombieInfo;
+    public GameObject KeysInfo;
     public InputField[] CharacterNameFields; //size 8
     public Text[] StocksTexts; //size 8
     public Text[] EnergyTexts; //size 8
@@ -22,8 +25,25 @@ public class HUDManager : MonoBehaviour
         var stFxs = new List<string>(Enum.GetNames(typeof(Enums.StatusEffect)));
         for (int i = 0; i < StFxDropdowns.Length; ++i)
         {
+            StFxDropdowns[i].ClearOptions();
             StFxDropdowns[i].AddOptions(stFxs);
-        }        
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Viking Info"))
+        {
+            VikingInfo.SetActive(!VikingInfo.activeSelf);
+        }
+        else if (Input.GetButtonDown("Zombie Info"))
+        {
+            ZombieInfo.SetActive(!ZombieInfo.activeSelf);
+        }
+        else if (Input.GetButtonDown("Keys Info"))
+        {
+            KeysInfo.SetActive(!KeysInfo.activeSelf);
+        }
     }
 
     public void Init(HUD hud)
@@ -41,30 +61,82 @@ public class HUDManager : MonoBehaviour
             StocksTexts[i].text = this.hud.Stocks[i].ToString();
             EnergyTexts[i].text = this.hud.EnergyPower[i].ToString();
         }
+
+        for (int i = 0; i < 16; ++i)
+        {
+            StFxDropdowns[i].value = this.hud.StFxs[i];
+            StFxTurnsTexts[i].text = this.hud.StFxsTurns[i].ToString();
+        }
     }
 
+#region UI calls
+    public void ChangeCharacterName(int index)
+    {
+        hud.ChangeCharacterName(index, CharacterNameFields[index].text);
+    }
+
+    public void AddStock(int index)
+    {
+        hud.AddStock(index);
+    }
+
+    public void RemoveStock(int index)
+    {
+        hud.RemoveStock(index);
+    }
+
+    public void AddEnergy(int index)
+    {
+        hud.AddEnergy(index);
+    }
+
+    public void RemoveEnergy(int index)
+    {
+        hud.RemoveEnergy(index);
+    }
+
+    public void ChangeStatusEffect(int index)
+    {
+        hud.ChangeStatusEffect(index, (Enums.StatusEffect)StFxDropdowns[index].value);
+    }
+
+    public void AddTurnsToStatusEffect(int index)
+    {
+        hud.AddTurnsToStatusEffect(index);
+    }
+
+    public void RemoveTurnsToStatusEffect(int index)
+    {
+        hud.RemoveTurnsToStatusEffect(index);
+    }
+#endregion
+
+#region events
     public void UpdateNames(int index)
     {
-
+        CharacterNameFields[index].text = hud.CharacterNames[index];
     }
 
     public void UpdateStocks(int index)
     {
-       
+        StocksTexts[index].text = hud.Stocks[index].ToString();
+        //StocksTexts[index].text = (int.Parse(StocksTexts[index].text) + 1).ToString();
     }
 
     public void UpdateEnergy(int index)
     {
-
+        EnergyTexts[index].text = hud.EnergyPower[index].ToString();
     }
 
     public void UpdateStFx(int index)
     {
-
+        StFxDropdowns[index].value = hud.StFxs[index];
     }
 
     public void UpdateStFxTurns(int index)
     {
-
+        StFxTurnsTexts[index].text = hud.StFxsTurns[index].ToString();
     }
+
+#endregion
 }

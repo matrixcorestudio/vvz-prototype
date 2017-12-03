3b43cf7809c35fbeca94484ca067b9364f197df3
Alfaman Ztriker <Ztrikerx21@gmail.com>
2017-12-03 03:02:04 -0800

    Se incorpora UI para leer status efx. La lista de los efectos aun no esta completa, pero todo es funcional. Falta hacerle resize a los paneles y al font para mejor quedar con la presentacion del juego.

---
+++ Assets/Resources/StatusEfx/Status.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Xml;
+using System.Xml.Serialization;
+
+public struct Status {
+
+    public string ID;
+	public string Name;
+	public string Parameter;
+	public string Target;
+	public string Clasification;
+	public string Description;
+	public string Troubleshooting;
+
+	public Status(string id, string name, string parameter, string target, string clasification, string description, string troubleshooting){
+		ID = id;
+		Name = name;
+		Parameter = parameter;
+		Target = target;
+		Clasification = clasification;
+		Description = description;
+		Troubleshooting = troubleshooting;
+	}
+
+    
+
+}
---
+++ Assets/Resources/StatusEfx/StatusDropdown.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatusDropdown : MonoBehaviour {
+
+	public Dropdown dropdown;
+	public Text selectedName;
+	public Text selectedText;
+	public XmlParser statusReference;
+
+	public void Dropdown_IndexChanged(int index){
+		string totVal = "";
+		index--;// Se hace el -- para contrarestar el espacio vacio en el dropdown
+		selectedName.text = statusReference.statusList[index].Name;
+		//selectedText.text = statusReference.statusList[index].Description;
+		
+		totVal += "parameter : "+ statusReference.statusList[index].Parameter+"\n target : "+statusReference.statusList[index].Target+"\n clasification : "+ statusReference.statusList[index].Clasification+"\n description : "+ statusReference.statusList[index].Description+"\n troubleshooting : "+ statusReference.statusList[index].Troubleshooting+"\n\n";
+		selectedText.text = totVal;
+	}
+
+	public void PopulateList(){
+		List<string> names = new List<string>();
+		int totalEntries =  statusReference.statusList.Count;
+		for (int i=0; i<totalEntries;i++ ){
+			names.Add ( statusReference.statusList[i].Name);
+
+		}
+		dropdown.AddOptions(names);
+	}
+
+
+
+
+}

f80afeb60a0c8ec7238123535e3f2f195e3c6b57
Luis Ochoa <ph.lwyz@gmail.com>
2017-12-09 22:06:00 -0800

    Arrow marker for direction.

---
+++ Assets/ChangeDirection.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class ChangeDirection : NetworkBehaviour
+{
+    public Sprite[] arrowSprites;
+
+    private Button[] arrowButtons;
+    private int[] indeces;
+
+    private void Start()
+    {
+        arrowButtons = GetComponentsInChildren<Button>();
+        indeces = new int[arrowButtons.Length];
+        for (int i = 0; i < indeces.Length; i++)
+        {
+            indeces[i] = 0;
+        }
+    }
+
+    [Command]
+    public void CmdRotateArrow(int index)
+    {
+        if (isLocalPlayer)
+        {
+            RpcRotateArrow(index);
+        }
+    }
+
+    [ClientRpc]
+    private void RpcRotateArrow(int index)
+    {
+        ++indeces[index];
+        indeces[index] %= 4;
+        arrowButtons[index].GetComponent<Image>().sprite = arrowSprites[index];
+    }
+}
